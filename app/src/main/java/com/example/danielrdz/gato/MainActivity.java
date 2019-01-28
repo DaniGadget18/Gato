c4bef5c9411e8a3b57b2d6e0f9adecd5e6efaf23
danielrdz <dani_181298@hotmail.com>
2019-01-27 23:09:22 -0600

    Fase 1 gato

---
+++ app/src/main/java/com/example/danielrdz/gato/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.danielrdz.gato;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button jugar;
+    TextView tempo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        jugar = findViewById(R.id.jugar);
+        tempo = findViewById(R.id.temporizador);
+
+        jugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                new CountDownTimer(10000, 1000)
+                {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        tempo.setText("Segundos faltantes: " + millisUntilFinished / 1000);
+                    }
+                    @Override
+                    public void onFinish() {
+                        Intent segunda = new Intent(v.getContext(), segundo.class);
+                        startActivityForResult(segunda, 0);
+                        finish();
+                    }
+                }.start();
+            }
+        });
+    }
+}
